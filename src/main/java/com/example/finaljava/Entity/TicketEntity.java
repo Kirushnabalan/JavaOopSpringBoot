bcadf876c289d8243e3b121833f79916db00ba1f
Kirushnabalan <kirushnabalan1803gmail.com>
2024-11-29 08:23:07 +0530

    add first code

---
+++ src/main/java/com/example/finaljava/Entity/TicketEntity.java
@@ -0,0 +1,13 @@
+package com.example.finaljava.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class TicketEntity {
+
+    @Id
+    private int id;
+
+
+}
